d0a9338941f6611e77ceb9bd08e66bfa48e6ed7d
mattjhooper <matt.j.hooper@googlemail.com>
2021-12-05 10:47:22 +0000

    Day 5: Hydrothermal Venture solution

---
+++ AdventOfCode_Day5_HydrothermalVenture/Line.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode_Day5_HydrothermalVenture
+{
+    internal class Line
+    {
+        private readonly Point _start;
+        private readonly Point _end;
+
+        public Line(string lineDefinition)
+        {
+            var parts = lineDefinition.Split(' ');
+            _start = GetPointFromPart(parts[0]);
+            _end = GetPointFromPart(parts[2]);
+        }
+
+        public override string ToString()
+        {
+            return $"{_start} => {_end}";
+        }
+
+        public IEnumerable<Point> GetPoints()
+        {
+            Point p = _start;
+
+            while (p != _end)
+            {
+                yield return p;
+                p.Offset(LineDirection);
+            }
+
+            yield return _end;
+        }
+
+        private Point GetPointFromPart(string part)
+        {
+            var coords = part.Split(',');
+            int x = int.Parse(coords[0]);
+            int y = int.Parse(coords[1]);
+
+            return new Point(x, y);
+        }
+
+        private int Direction(int a, int b) => a == b ? 0 : a < b ? 1 : -1;
+
+        private Point LineDirection => new Point(Direction(_start.X, _end.X), Direction(_start.Y, _end.Y));  
+    }
+}
---
+++ AdventOfCode_Day5_HydrothermalVenture/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AdventOfCode_Day5_HydrothermalVenture;
+using System.Drawing;
+
+Console.WriteLine("--- Day 5: Hydrothermal Venture ---");
+
+string[] lines = System.IO.File.ReadAllLines(@"Input.txt");
+
+Dictionary<Point,int> vents = new ();
+
+foreach (string lineDefinition in lines)
+{
+    var line = new Line(lineDefinition);
+
+    foreach (var point in line.GetPoints())
+    {      
+        AddToVents(point);     
+    }    
+}
+
+int dangerPoints = vents.Where(v => v.Value >= 2).Count();
+
+Console.WriteLine($"Danger areas: {dangerPoints}");
+
+// Keep the console window open in debug mode.
+Console.WriteLine("Press any key to exit.");
+System.Console.ReadKey();
+
+void AddToVents(Point point)
+{
+    var count = vents.GetValueOrDefault(point, 0);
+    vents[point] = count + 1;
+}
