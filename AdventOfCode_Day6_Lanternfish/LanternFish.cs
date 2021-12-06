0b93cab1d101f65df6f46fdcabc4646ef8cf34dd
mattjhooper <matt.j.hooper@googlemail.com>
2021-12-06 14:22:11 +0000

    LanternFish part 1

---
+++ AdventOfCode_Day6_Lanternfish/LanternFish.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode_Day6_Lanternfish
+{
+    internal class LanternFish
+    {
+        public LanternFish(string timerStart)
+        {
+            Timer = int.Parse(timerStart);
+        }
+
+        public int Timer { get; private set; }
+
+        public void CheckTimer(List<LanternFish> newFish)
+        {
+            if (Timer == 0)
+            {
+                newFish.Add(new LanternFish("8"));
+                Timer = 6;
+                return;
+            }
+
+            Timer--;
+        }
+    }
+}
---
+++ AdventOfCode_Day6_Lanternfish/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AdventOfCode_Day6_Lanternfish;
+
+Console.WriteLine("--- Day 6: Lanternfish ---");
+
+string input = System.IO.File.ReadAllText(@"Input.txt");
+
+var timers = input.Split(',');
+
+List<LanternFish> shoal = new();
+
+foreach (var timer in timers)
+{
+    //Console.WriteLine(timer);
+    shoal.Add(new LanternFish(timer));
+
+}
+
+for (int day = 1; day <= 80; day++)
+{
+    List<LanternFish> newFish = new();
+
+    foreach(var fish in shoal)
+    {
+        fish.CheckTimer(newFish);
+    }
+
+    shoal.AddRange(newFish);
+}
+
+Console.WriteLine($"There are now {shoal.Count} lanternfish");
