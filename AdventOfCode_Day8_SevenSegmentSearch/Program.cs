51a16f9b1f5cee801c173045b77760dafcfa6734
mattjhooper <matt.j.hooper@googlemail.com>
2021-12-08 22:06:40 +0000

    Add FindDigit utility

--- AdventOfCode_Day8_SevenSegmentSearch/Program.cs
+++ AdventOfCode_Day8_SevenSegmentSearch/Program.cs
@@ -9,20 +9,20 @@ foreach (string line in lines)
 {
     var parts = line.Split('|');
 
-    HashSet<char>[] signalSets = Array.ConvertAll<string, HashSet<char>>(parts[0].Trim(' ').Split(' '), (s) => new HashSet<char>(s.ToCharArray()));
+    List<HashSet<char>> signalSets = new List<HashSet<char>>(Array.ConvertAll<string, HashSet<char>>(parts[0].Trim(' ').Split(' '), (s) => new HashSet<char>(s.ToCharArray())));
 
-    HashSet<char>[] digits = new HashSet<char>[signalSets.Length];
+    HashSet<char>[] digits = new HashSet<char>[signalSets.Count];
 
-    digits[1] = signalSets.First(d => d.Count == 2);
-    digits[7] = signalSets.First(d => d.Count == 3);
-    digits[4] = signalSets.First(d => d.Count == 4);
-    digits[8] = signalSets.First(d => d.Count == 7);
-    digits[3] = signalSets.First(d => d.Count == 5 && d.IsProperSupersetOf(digits[7]));
-    digits[9] = signalSets.First(d => d.Count == 6 && d.IsProperSupersetOf(digits[3]));
-    digits[0] = signalSets.First(d => d.Count == 6 && !d.SetEquals(digits[9]) && d.IsProperSupersetOf(digits[1]));
-    digits[6] = signalSets.First(d => d.Count == 6 && !d.SetEquals(digits[0]) && !d.SetEquals(digits[9]));
-    digits[5] = signalSets.First(d => d.Count == 5 && d.IsProperSubsetOf(digits[6]));
-    digits[2] = signalSets.First(d => d.Count == 5 && !d.SetEquals(digits[3]) && !d.SetEquals(digits[5]));
+    digits[1] = signalSets.FindDigit(d => d.Count == 2);
+    digits[7] = signalSets.FindDigit(d => d.Count == 3);
+    digits[4] = signalSets.FindDigit(d => d.Count == 4);
+    digits[8] = signalSets.FindDigit(d => d.Count == 7);
+    digits[3] = signalSets.FindDigit(d => d.Count == 5 && d.IsProperSupersetOf(digits[7]));
+    digits[9] = signalSets.FindDigit(d => d.Count == 6 && d.IsProperSupersetOf(digits[3]));
+    digits[0] = signalSets.FindDigit(d => d.Count == 6 && d.IsProperSupersetOf(digits[1]));
+    digits[6] = signalSets.FindDigit(d => d.Count == 6);
+    digits[5] = signalSets.FindDigit(d => d.Count == 5 && d.IsProperSubsetOf(digits[6]));
+    digits[2] = signalSets.FindDigit(d => d.Count == 5);
 
     HashSet<char>[] outputValues = Array.ConvertAll<string, HashSet<char>>(parts[1].Trim(' ').Split(' '), (s) => new HashSet<char>(s.ToCharArray()));
 
@@ -37,3 +37,13 @@ foreach (string line in lines)
 }
 
 Console.WriteLine($"Total: {total}"); // 1043697
+
+public static class Utils
+{
+    public static HashSet<char> FindDigit(this List<HashSet<char>> signalSets, Func<HashSet<char>, bool> predicate)
+    {
+        var digit = signalSets.Single(predicate);
+        signalSets.Remove(digit);
+        return digit;
+    }
+}
