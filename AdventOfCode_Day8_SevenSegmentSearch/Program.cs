9cb491ca8ece2f388b1af9f43ce00fe0b3b25600
mattjhooper <matt.j.hooper@googlemail.com>
2021-12-08 08:21:13 +0000

    Day 8 - Example

---
+++ AdventOfCode_Day8_SevenSegmentSearch/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("--- Day 8: Seven Segment Search ---");
+
+string[] lines = System.IO.File.ReadAllLines(@"Input.txt");
+
+int digitCount = 0;
+
+foreach (string line in lines)
+{
+    var parts = line.Split('|');
+
+    var outputValues = parts[1].Trim(' ').Split(' ');
+
+    Console.WriteLine($"--- New Line ----");
+    foreach (var outputValue in outputValues)
+    {
+        if (outputValue.Length != 5 && outputValue.Length != 6)
+        {
+            digitCount++;
+            Console.WriteLine($"Value: {outputValue}. Length: {outputValue.Length}");
+        }
+    }
+}
+
+Console.WriteLine($"Interesting Digits: {digitCount}");
