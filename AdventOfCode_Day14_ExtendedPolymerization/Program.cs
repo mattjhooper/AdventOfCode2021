f3c040aa3a9c9488cb9359494e7c8b698d25ecd8
mattjhooper <matt.j.hooper@googlemail.com>
2021-12-15 07:37:08 +0000

    Day 14 - Part 2

--- AdventOfCode_Day14_ExtendedPolymerization/Program.cs
+++ AdventOfCode_Day14_ExtendedPolymerization/Program.cs
@@ -5,49 +5,99 @@ string[] lines = System.IO.File.ReadAllLines(@"Input.txt");
 
 string polymer = lines[0];
 
-Dictionary<string, string> map = new Dictionary<string, string>();
+Dictionary<string, InsertionRule> map = new ();
 
 for (int i = 2; i < lines.Length; i++)
 {
     string key = lines[i].Substring(0, 2);
-    string value = $"{key[0]}{lines[i].Substring(lines[i].Length - 1, 1)}{key[1]}";
+    string child1 = $"{key[0]}{lines[i].Substring(lines[i].Length - 1, 1)}";
+    string child2 = $"{lines[i].Substring(lines[i].Length - 1, 1)}{key[1]}";
 
-    map.Add(key, value);
+    InsertionRule rule = new InsertionRule { Child1 = child1, Child2 = child2, Count = 0 };
+
+    map.Add(key, rule);
 }
 
-/*
-foreach(var item in map)
+for (int i = 0; i < polymer.Length - 1; i++)
 {
-    Console.WriteLine($"{item.Key} -> {item.Value}");
-}*/
+    map[polymer.Substring(i, 2)].Count++;
+}
 
-for (int loop = 0; loop < 10; loop++)
+for (int loop = 0; loop < 40; loop++)
 {
+    var tempMap = map.ToDictionary(v => v.Key, v => v.Value.Clone());
 
-    List<string> parts = new List<string>();
+    map.Reset();
 
-    for (int i = 0; i < polymer.Length - 1; i++)
+    foreach (var entry in tempMap)
     {
-        parts.Add(polymer.Substring(i, 2));
+        map[entry.Value.Child1].Count += entry.Value.Count;
+        map[entry.Value.Child2].Count += entry.Value.Count;
     }
 
-    for (int i = 0; i < parts.Count; i++)
+}
+
+map.Print();
+
+Console.WriteLine($"Total Count: {map.Sum(e => e.Value.Count)}");
+
+var charCount = map.GetCharacterCount(polymer[polymer.Length - 1]);
+
+long highestCount = charCount.Max(c => charCount[c.Key]);
+long lowestCount = charCount.Min(c => charCount[c.Key]);
+
+Console.WriteLine($"{highestCount} - {lowestCount} = {highestCount - lowestCount}"); // 1749 - 161 = 1588
+
+
+public class InsertionRule
+{
+    public long Count { get; set; }
+    public string Child1 { get; set; }
+    public string Child2 { get; set; }
+
+    public InsertionRule Clone() => new InsertionRule { Child1 = this.Child1, Child2 = this.Child2, Count = this.Count }; 
+}
+
+public static class Utils
+{
+    public static void Reset(this Dictionary<string, InsertionRule> map)
     {
-        parts[i] = map[parts[i]];
+        foreach (var entry in map)
+        {
+            entry.Value.Count = 0;
+        }
     }
 
-    polymer = polymer[0].ToString() + parts.Aggregate("", (a, b) => a + b.Substring(1, 2));
+    public static void Print(this Dictionary<string, InsertionRule> map)
+    {
+        foreach (var item in map)
+        {
+            Console.WriteLine($"{item.Key} -> {item.Value.Child1} + {item.Value.Child2}. Count: {item.Value.Count}");
+        }
+    }
 
-   // Console.WriteLine(polymer);
+    public static Dictionary<char, long> GetCharacterCount(this Dictionary<string, InsertionRule> map, char extraChar)
+    {
+        Dictionary<char, long> charCount = new();
+        foreach (var item in map)
+        {
+            char c1 = item.Key[0];
+            char c2 = item.Key[1];
+
+            charCount[c1] = charCount.GetValueOrDefault(c1, 0) + item.Value.Count;
+            charCount[c2] = charCount.GetValueOrDefault(c2, 0) + item.Value.Count;
+        }
+
+        // halve each entry
+        foreach(var entry in charCount)
+        {
+            charCount[entry.Key] = entry.Value / 2;
+        }
+
+        // Add extra character (the one on the end of the original polymer)
+        charCount[extraChar]++;
+
+        return charCount;
+    }
+
 }
-
-Dictionary<char, int> charCount = new();
-foreach (char c in polymer)
-{
-    charCount[c] = charCount.GetValueOrDefault(c, 0) + 1;
-}
-
-int highestCount = charCount.Max(c => charCount[c.Key]);
-int lowestCount = charCount.Min(c => charCount[c.Key]);
-
-Console.WriteLine($"{highestCount} - {lowestCount} = {highestCount - lowestCount}");
